abf6502d3a03e813130b4af6e53bd52c89cc1abf
Prakash Singh <95623466+Prakashgolusingh@users.noreply.github.com>
2023-10-05 11:22:53 +0530

    Add files via upload

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def about1
+  end
+  def about2
+  end
+  def about3
+  end
+  def about4
+  end
+  def about5
+  end
+  def about6
+  end
+  def about7
+  end
+  def about8
+  end
+  def about9
+  end
+  def about10
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root 'home#about1'
+  get 'home/about2'
+  get 'home/about3'
+  get 'home/about4'
+  get 'home/about5'
+  get 'home/about6'
+  get 'home/about7'
+  get 'home/about8'
+  get 'home/about9'
+  get 'home/about10'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
